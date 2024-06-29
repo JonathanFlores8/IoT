e869a4796fadf9d4ec390962dad0e9f18bd43910
JonathanTobiasFlores <jonathan.tobias.flores@gmail.com>
2024-06-29 17:41:28 +0200

    Add local project to github

---
+++ config.example.py
@@ -0,0 +1,6 @@
+# This file contains the example configuration for the project. Fill in the values and rename the file to config.py
+
+WIFI_SSID = ''
+WIFI_PASSWORD = ''
+AIO_USERNAME = ''
+AIO_KEY = ''
